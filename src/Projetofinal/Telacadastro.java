97611c50e83ee283bb35deacb2f000b0963bc4fb
aluno <aluno@10.61.7.8>
2026-02-25 10:26:39 -0300

    push 7

--- src/Projetofinal/Telacadastro.java
+++ src/Projetofinal/Telacadastro.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.JInternalFrame;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
 
 public class Telacadastro extends JFrame {
 
@@ -32,10 +37,21 @@ public class Telacadastro extends JFrame {
 	 */
 	public Telacadastro() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1212, 1010);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		FlowLayout flowLayout_1 = (FlowLayout) contentPane.getLayout();
+		flowLayout_1.setHgap(280);
+		flowLayout_1.setVgap(200);
+		contentPane.setBackground(new Color(106, 143, 123));
+		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(UIManager.getBorder("ComboBox.editorBorder"));
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setVgap(200);
+		flowLayout.setHgap(280);
+		contentPane.add(panel);
 
 	}
 
